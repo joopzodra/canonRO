9317b07199099557f326799b01bbcfbd3b8ccb28
Joop Rosier <joop@zodra.nl>
2017-04-15 16:58:05 +0200

    entryIdTransformDict changed, it expects complete data (data entries length to be equal to entryIdTransformDict length; captions removed from image slider

--- src/helpers/entryIdTransformDict.ts
+++ src/helpers/entryIdTransformDict.ts
@@ -1,10 +1,26 @@
 //The dictionary keys correspond with the entry numbers in the Canon RO book
 
 export const entryIdTransformDict = {
-  1: { entriesIndex: 5, idApp: 26, title: 'Randstad & Groene Hart' },
-  2: { entriesIndex: 3, idApp: 13, title: 'IJsselmeerpolders' },
-  3: { entriesIndex: 4, idApp: 20, title: 'Nagele' },
-  4: { entriesIndex: 0, idApp: 4, title: '‘Blokjeskaart’' },
-  5: { entriesIndex: 2, idApp: 10, title: 'Groeikern Houten' },
-  6: { entriesIndex: 1, idApp: 9, title: 'Ecologische Hoofdstructuur' }
+  10: { appId: 1, title: 'Amsterdam Zuid Berlage' },
+  16: { appId: 2, title: 'AUP Amsterdam' },
+  4: { appId: 4, title: '‘Blokjeskaart’' },
+  9: { appId: 5, title: 'Bolwerkparken van Zwolle' },
+  19: { appId: 8, title: 'Deltawerken' },
+  6: { appId: 9, title: 'Ecologische Hoofdstructuur' },
+  5: { appId: 10, title: 'Groeikern Houten' },
+  2: { appId: 13, title: 'IJsselmeerpolders' },
+  13: { appId: 14, title: 'Knooppunt Oudenrijn' },
+  17: { appId: 15, title: 'Kootwijkerzand' },
+  18: { appId: 16, title: 'Lijnbaan Rotterdam' },
+  21: { appId: 18, title: 'Midden-Delfland' },
+  15: { appId: 19, title: 'Naardermeer' },
+  3: { appId: 20, title: 'Nagele' },
+  8: { appId: 22, title: 'Nieuwe Hollandse Waterlinie' },
+  7: { appId: 25, title: 'Pieterpad' },
+  1: { appId: 26, title: 'Randstad & Groene Hart' },
+  12: { appId: 28, title: 'Rijksstraatwegenplan' },
+  20: { appId: 30, title: 'Stokstraat Maastricht' },
+  11: { appId: 32, title: 'Tuindorp Heveadorp' },
+  14: { appId: 35, title: 'Zuid-Willemsvaart' }
+
 };
